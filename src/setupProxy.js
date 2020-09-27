aabe7bde3defece3befadad7b062625779404856
kazura <2233086283@qq.com>
2020-09-27 19:06:27 +0800

    配置代理

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:8080',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '',
+      },
+    })
+  )
+}
