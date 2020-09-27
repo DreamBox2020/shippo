56f3e3851aa01a91ab3fcde0b5c681bfa304582a
kazura233 <kazira233@gmail.com>
2020-09-27 19:50:42 +0800

    增加路由

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import './utils/bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter } from 'react-router-dom'
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+)
