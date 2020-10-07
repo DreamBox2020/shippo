080d1b3ccbe7f5d23f3f6b2690fff63a58cb691d
kazura233 <kazira233@gmail.com>
2020-10-08 04:29:24 +0800

    个人主页雏形完毕

--- src/layouts/space/index.tsx
+++ src/layouts/space/index.tsx
@@ -1,7 +1,69 @@
+import { Icon, List, NavBar } from 'antd-mobile'
 import React from 'react'
+import { Container, Header, Main } from '~/components'
+
+const { Item } = List
+const { Brief } = Item
 
 export const Space = () => {
-  return <h1>Space</h1>
+  return (
+    <Container direction="vertical">
+      <Header height="45px">
+        <NavBar
+          mode="light"
+          icon={<Icon type="left" />}
+          onLeftClick={() => console.log('onLeftClick')}
+          rightContent={[
+            <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+            <Icon key="1" type="ellipsis" />,
+          ]}
+        >
+          NavBar
+        </NavBar>
+      </Header>
+      <Main>
+        <List renderHeader={() => '被赞9999+次，被收藏9999+次。'} className="my-list">
+          <div>
+            <img width="100%" src={require('~/assets/bg.jpg')} alt="" />
+          </div>
+          <Item arrow="horizontal" multipleLine onClick={() => {}}>
+            Title <Brief>subtitle</Brief>
+          </Item>
+          <Item arrow="horizontal" multipleLine onClick={() => {}} platform="android">
+            ListItem （Android）
+            <Brief>
+              There may have water ripple effect of <br /> material if you set the click event.
+            </Brief>
+          </Item>
+          <Item
+            arrow="horizontal"
+            thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+            multipleLine
+            onClick={() => {}}
+          >
+            Title <Brief>subtitle</Brief>
+          </Item>
+          <Item arrow="horizontal" multipleLine onClick={() => {}}>
+            Title <Brief>subtitle</Brief>
+          </Item>
+          <Item arrow="horizontal" multipleLine onClick={() => {}} platform="android">
+            ListItem （Android）
+            <Brief>
+              There may have water ripple effect of <br /> material if you set the click event.
+            </Brief>
+          </Item>
+          <Item
+            arrow="horizontal"
+            thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+            multipleLine
+            onClick={() => {}}
+          >
+            Title <Brief>subtitle</Brief>
+          </Item>
+        </List>
+      </Main>
+    </Container>
+  )
 }
 
 export default Space
