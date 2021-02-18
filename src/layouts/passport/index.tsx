5acbf31c92e3d22ae4ea5de56d7f8aae83ca2f80
kazura233 <kazira233@gmail.com>
2021-02-18 21:42:19 +0800

    登陆页面

--- src/layouts/passport/index.tsx
+++ src/layouts/passport/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import {Passport as Page} from '~/pages/passport'
+import { Passport as Page } from '~/pages/passport'
 
 export const Passport = () => {
-    return <Switch>
-        <Route key="login" path="/passport" exact={true} component={Page}></Route>
+  return (
+    <Switch>
+      <Route key="login" path="/passport" exact={true} component={Page}></Route>
     </Switch>
+  )
 }
--- src/pages/passport/index.tsx
+++ src/pages/passport/index.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import { Button, Flex, InputItem, WhiteSpace, WingBlank } from 'antd-mobile'
+import React, { useState } from 'react'
+import { COLOR_PINK } from '~/constants/config'
 
 export const Passport = () => {
-    return <h1>Passport/Login</h1>
+  const [phone, setPhone] = useState('')
+  const [code, setCode] = useState('')
+
+  const callLogon = () => {
+    console.log('callLogon', { phone, code })
+  }
+
+  return (
+    <div>
+      <InputItem
+        type="phone"
+        placeholder="请输入手机号码"
+        onChange={(value) => setPhone(value)}
+        value={phone}
+      >
+        手机号码
+      </InputItem>
+
+      <Flex>
+        <InputItem
+          style={{ flex: 3 }}
+          type="number"
+          placeholder="请输入验证码"
+          onChange={(value) => setCode(value)}
+          value={code}
+        >
+          验证码
+        </InputItem>
+        <Button style={{ flex: 1 }}>获取验证码</Button>
+      </Flex>
+
+      <WhiteSpace size="lg" />
+
+      <WingBlank>
+        <Button style={{ background: COLOR_PINK, color: '#fff' }} onClick={callLogon}>
+          登陆
+        </Button>
+      </WingBlank>
+    </div>
+  )
 }
 
 export default Passport
