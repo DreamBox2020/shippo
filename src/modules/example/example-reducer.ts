c9c6b0eae7cf87db86bf111cbf6191d0a6ffaf7b
kazura233 <kazira233@gmail.com>
2021-04-04 22:17:28 +0800

    移除example的reducer文件

--- src/modules/example/example-reducer.ts
+++
@@ -1,8 +0,0 @@
-// import { createReducer } from '../util'
-// import { exampleStore, IExampleStore } from './example-store'
-
-// const reducer = createReducer<IExampleStore>('example')
-
-// export const exampleReducer = reducer(exampleStore)
-
-export {}
