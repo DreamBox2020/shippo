fb0443d2652613cb8a12da77426194e1dc72a467
kazura233 <kazira233@gmail.com>
2021-08-13 00:17:37 +0800

    增加vconsole

--- src/components/root/index.tsx
+++ src/components/root/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 import { rootStore } from '~/modules'
 import { GlobalStyle } from '~/styles/global'
 import RootRoute from './root-router'
+import VConsole from 'vconsole'
 
 export const Root = () => {
+  useEffect(() => {
+    new VConsole()
+  }, [])
+
   return (
     <Provider store={rootStore}>
       <GlobalStyle></GlobalStyle>
