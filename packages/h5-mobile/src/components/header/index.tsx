ec1c4e2dd5991e7b1ace13521f6deeb60859e853
kazura233 <kazira233@gmail.com>
2021-09-15 22:32:19 +0800

    :tada:Header组件props支持style

--- packages/h5-mobile/src/components/header/index.tsx
+++ packages/h5-mobile/src/components/header/index.tsx
@@ -8,11 +8,12 @@ const StyledHeader = styled.header`
 
 export interface IHeaderProps {
   height?: string
+  style?: React.CSSProperties
 }
 
 export const Header: React.FC<IHeaderProps> = (props) => {
-  const { height = '60px', children } = props
-  return <StyledHeader style={{ height }}>{children}</StyledHeader>
+  const { height = '60px', children, style } = props
+  return <StyledHeader style={{ height, ...style }}>{children}</StyledHeader>
 }
 
 export default Header
