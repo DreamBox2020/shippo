2f7ad3867f79e87956ec1af2256881557c4fed9c
kazura233 <kazira233@gmail.com>
2021-09-16 02:41:16 +0800

    :tada:个人中心页面雏形

--- packages/h5-mobile/src/layouts/space/index.tsx
+++ packages/h5-mobile/src/layouts/space/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Container from '~/components/container'
+import Header from '~/components/header'
+import Main from '~/components/main'
+import { WhiteSpace } from '~/components/white-space'
 
 export const Space = () => {
-  return <div>Space</div>
+  return (
+    <Container direction="vertical">
+      <Header
+        style={{
+          height: '45px',
+          lineHeight: '45px',
+          backgroundColor: '#fff',
+          textAlign: 'center',
+          fontSize: '18px',
+        }}
+      >
+        个人中心
+      </Header>
+      <WhiteSpace size={15} />
+      <Main></Main>
+    </Container>
+  )
 }
 
 export default Space
