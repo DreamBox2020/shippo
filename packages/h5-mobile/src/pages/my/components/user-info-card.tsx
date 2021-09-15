988e2b825e81d36a9795b0192f25589f97b14e52
kazura233 <kazira233@gmail.com>
2021-09-16 02:41:01 +0800

    :tada:我页面雏形

--- packages/h5-mobile/src/pages/my/components/user-info-card.tsx
+++ packages/h5-mobile/src/pages/my/components/user-info-card.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useHistory } from 'react-router-dom'
-import { Card } from 'antd-mobile'
+import { List, Image } from 'antd-mobile'
 import { Level } from '~/components/level'
-
-const StyledUserInfoCard = styled(Card)``
 
 const StyledInfoBlock = styled.span`
   font-size: 12px;
@@ -16,21 +14,28 @@ export const UserInfoCard = () => {
   const history = useHistory()
 
   return (
-    <StyledUserInfoCard
-      title={
-        <div>
-          <div>
-            系统保留内测专用帐号<Level>9</Level>
-          </div>
+    <List>
+      <List.Item
+        prefix={
+          <Image
+            src={require('~/assets/avatar.png').default}
+            style={{ borderRadius: '25px' }}
+            fit="cover"
+            width="50px"
+            height="50px"
+          />
+        }
+        description={
           <p>
             <StyledInfoBlock>经验：99999</StyledInfoBlock>
             <StyledInfoBlock>积分：99999</StyledInfoBlock>
           </p>
-        </div>
-      }
-      // thumb={require('~/assets/avatar.png').default}
-      // extra={<Icon type="right" size="md" onClick={() => history.push('/space/100000')} />}
-    />
+        }
+        onClick={() => history.push('/space/100000')}
+      >
+        内测账号001<Level>9</Level>
+      </List.Item>
+    </List>
   )
 }
 
--- packages/h5-mobile/src/pages/my/index.tsx
+++ packages/h5-mobile/src/pages/my/index.tsx
@@ -1,23 +1,44 @@
 import React from 'react'
-import { Card } from 'antd-mobile'
+import { List } from 'antd-mobile'
 import { Container } from '~/components/container'
 import { Header } from '~/components/header'
 import { Main } from '~/components/main'
+import { WhiteSpace } from '~/components/white-space'
 import { UserInfoCard } from './components/user-info-card'
+import { BellOutlined, ProfileOutlined, SettingOutlined } from '@ant-design/icons'
+import { useHistory } from 'react-router'
 
 export const My = () => {
+  const history = useHistory()
+
   return (
     <Container direction="vertical">
-      <Header height="45px">{/* <NavBar mode="light">我</NavBar> */}</Header>
+      <Header
+        style={{
+          height: '45px',
+          lineHeight: '45px',
+          backgroundColor: '#fff',
+          textAlign: 'center',
+          fontSize: '18px',
+        }}
+      >
+        我
+      </Header>
+      <WhiteSpace size={15} />
       <Main>
-        <Card>
-          <UserInfoCard />
-          <div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        </Card>
+        <UserInfoCard />
+        <WhiteSpace size={15} />
+        <List>
+          <List.Item prefix={<BellOutlined />} onClick={() => {}}>
+            通知
+          </List.Item>
+          <List.Item prefix={<ProfileOutlined />} onClick={() => {}}>
+            我的作品
+          </List.Item>
+          <List.Item prefix={<SettingOutlined />} onClick={() => history.push('/setting')}>
+            设置
+          </List.Item>
+        </List>
       </Main>
     </Container>
   )
