978e525846cc5b3970fba7c7bd34aa00699bfcb5
kazura233 <kazira233@gmail.com>
2021-09-16 02:40:29 +0800

    :tada:首页页面雏形

--- packages/h5-mobile/src/pages/home/index.tsx
+++ packages/h5-mobile/src/pages/home/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Container from '~/components/container'
+import Header from '~/components/header'
+import Main from '~/components/main'
+import { WhiteSpace } from '~/components/white-space'
 
 export const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <Container direction="vertical">
+      <Header
+        style={{
+          height: '45px',
+          lineHeight: '45px',
+          backgroundColor: '#fff',
+          textAlign: 'center',
+          fontSize: '18px',
+        }}
+      >
+        首页
+      </Header>
+      <WhiteSpace size={15} />
+      <Main></Main>
+    </Container>
+  )
 }
 
 export default Home
