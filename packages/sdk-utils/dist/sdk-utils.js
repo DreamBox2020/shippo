a12a8d9e6e50b1a90c97360e9ec480a42a6fcfd7
kazura233 <kazira233@gmail.com>
2021-09-17 10:51:03 +0800

    :tada:utils迁移到sdk

--- packages/sdk-utils/dist/sdk-utils.js
+++ packages/sdk-utils/dist/sdk-utils.js
@@ -58,7 +58,95 @@
       return Request;
   }());
 
+  /**
+   * 检查手机号是否合规
+   * @param phone
+   * @returns
+   */
+  var checkPhone = function (phone) {
+      var list = [
+          // 移动
+          // '134', // 0～8
+          '135',
+          '136',
+          '137',
+          '138',
+          '139',
+          // '147', // 数据卡
+          // '148', // 数据卡
+          '150',
+          '151',
+          '152',
+          // '157', // 固话卡
+          '158',
+          '159',
+          // '172', // 数据卡
+          '178',
+          '182',
+          '183',
+          '184',
+          '187',
+          '188',
+          '195',
+          '197',
+          '198',
+          // 联通
+          '130',
+          '131',
+          '132',
+          // '145', // 数据卡
+          // '146', // 数据卡
+          '155',
+          '156',
+          '166',
+          // '171', // 副号卡
+          '175',
+          '176',
+          '185',
+          '186',
+          '196',
+          //电信
+          '133',
+          // '149', // 数据卡
+          '153',
+          '173',
+          '177',
+          '180',
+          '181',
+          '189',
+          '190',
+          '191',
+          '193',
+          '199',
+      ];
+      // 如果不是1～9开头，且不是11位数字
+      if (!/^[1-9][0-9]{10}$/.test(phone)) {
+          return false;
+      }
+      // 如果是134开头，且第四位在0～8之间
+      if (/^134[0-8]/.test(phone)) {
+          return true;
+      }
+      // 循环判断开头
+      for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+          var val = list_1[_i];
+          if (phone.startsWith(val))
+              return true;
+      }
+      return false;
+  };
+  /**
+   * 检查短信验证码是否合规
+   * @param code
+   * @returns
+   */
+  var checkSmsCode = function (code) {
+      return /^[1-9][0-9]{5}$/.test(code);
+  };
+
   exports.Request = Request;
+  exports.checkPhone = checkPhone;
+  exports.checkSmsCode = checkSmsCode;
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
---
+++ packages/sdk-utils/types/checks.d.ts
@@ -0,0 +1,12 @@
+/**
+ * 检查手机号是否合规
+ * @param phone
+ * @returns
+ */
+export declare const checkPhone: (phone: string) => boolean;
+/**
+ * 检查短信验证码是否合规
+ * @param code
+ * @returns
+ */
+export declare const checkSmsCode: (code: string) => boolean;
