ed00d88a528b9ba92c6c24b062d5eb7a68c847f8
kazura233 <kazira233@gmail.com>
2021-09-18 10:02:44 +0800

    :bug:action名称错误

--- packages/sdk-stores/src/user/user-action.ts
+++ packages/sdk-stores/src/user/user-action.ts
@@ -1,6 +1,6 @@
 import { createAction } from '@kazura/react-store'
 import { IUserStore } from './user-reducer'
 
-const action = createAction<IUserStore>('example')
+const action = createAction<IUserStore>('user')
 
 export const userUpdateInfo = action((info: { uid: number }) => ({ info }))
