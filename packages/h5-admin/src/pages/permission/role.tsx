4bb2fa531187b2bb49e9845bacc25848aff545ac
kazura233 <kazira233@gmail.com>
2022-04-04 01:25:06 +0800

    角色设置页面

--- packages/h5-admin/src/pages/permission/role.tsx
+++ packages/h5-admin/src/pages/permission/role.tsx
@@ -1,69 +1,114 @@
-import { Button, Space, Table } from 'antd'
-import { useState } from 'react'
-
-const columns = [
-  {
-    title: '角色ID',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: '角色名称',
-    dataIndex: 'roleName',
-    key: 'roleName',
-  },
-  {
-    title: '描述',
-    dataIndex: 'remark',
-    key: 'remark',
-  },
-  {
-    title: '创建时间',
-    dataIndex: 'createdAt',
-    key: 'createdAt',
-  },
-  {
-    title: '操作',
-    key: 'action',
-    render: () => (
-      <Space size="middle">
-        <Button type="link">修改</Button>
-        <Button type="link">删除</Button>
-        <Button type="link">权限策略配置</Button>
-      </Space>
-    ),
-  },
-]
-
-const data = [
-  {
-    key: 1,
-    id: 1,
-    roleName: 'admin',
-    remark: '管理员',
-    createdAt: '2022-01-01 22:22:22',
-  },
-  {
-    key: 2,
-    id: 2,
-    roleName: 'user',
-    remark: '用户',
-    createdAt: '2022-01-01 22:22:22',
-  },
-  {
-    key: 3,
-    id: 3,
-    roleName: 'root',
-    remark: '系统超级用户',
-    createdAt: '2022-01-01 22:22:22',
-  },
-]
+import { useState, useRef, useCallback } from 'react'
+import { useMount } from 'ahooks'
+import { Button, Space, Table, Modal, message } from 'antd'
+import { ColumnsType } from 'antd/lib/table'
+import { formatTimeStr } from '@shippo/sdk-utils'
+import { services, IRole } from '@shippo/sdk-services'
+import { EditRoleDrawer, EditRoleDrawerRef } from './components/edit-role-drawer'
+import { EditRolePolicyDrawer, EditRolePolicyDrawerRef } from './components/edit-role-policy-drawer'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
+const { confirm } = Modal
 
 export const Page_permission_role: React.FC = () => {
+  const [data, setData] = useState<(IRole & { key: string })[]>()
+  const editRoleDrawerRef = useRef<EditRoleDrawerRef>(null)
+  const editRolePolicyDrawerRef = useRef<EditRolePolicyDrawerRef>(null)
+
+  const handleDle = useCallback((id: number) => {
+    confirm({
+      title: '确认删除？',
+      icon: <ExclamationCircleOutlined />,
+      content: '此操作不可逆',
+      onOk() {
+        console.log('OK')
+        services.role.del({ id }).then((hr) => {
+          if (hr.data.success) {
+            message.success('成功')
+          } else {
+            message.success('失败')
+          }
+        })
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+    })
+  }, [])
+
+  const [columns, setColumns] = useState<ColumnsType<IRole>>([
+    {
+      title: '角色ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: '角色名称',
+      dataIndex: 'roleName',
+      key: 'roleName',
+    },
+    {
+      title: '描述',
+      dataIndex: 'remark',
+      key: 'remark',
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: (_, record) => {
+        return (
+          <Space size="middle">
+            <Button
+              type="link"
+              onClick={() => {
+                editRoleDrawerRef.current?.open(record)
+              }}
+            >
+              修改
+            </Button>
+            <Button
+              type="link"
+              onClick={() => {
+                handleDle(record.id)
+              }}
+            >
+              删除
+            </Button>
+            <Button
+              type="link"
+              onClick={() => {
+                editRolePolicyDrawerRef.current?.open(record)
+              }}
+            >
+              权限策略配置
+            </Button>
+          </Space>
+        )
+      },
+    },
+  ])
+
+  useMount(async () => {
+    const hr = await services.role.find_all()
+    setData(
+      hr.data.resource.map((role) => {
+        return { ...role, createdAt: formatTimeStr(role.createdAt), key: String(role.id) }
+      })
+    )
+  })
+
   return (
     <div>
+      <EditRoleDrawer ref={editRoleDrawerRef} />
+      <EditRolePolicyDrawer ref={editRolePolicyDrawerRef} />
       <Space size="middle">
-        <Button type="primary">新增角色</Button>
+        <Button type="primary" onClick={() => editRoleDrawerRef.current?.open()}>
+          新增角色
+        </Button>
       </Space>
       <Table columns={columns} dataSource={data} pagination={{ position: ['bottomCenter'] }} />
     </div>
