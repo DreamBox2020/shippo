8632194c76ebd4ecf9ad5bf47a9a47d47a8e45c7
kazura233 <kazira233@gmail.com>
2022-04-10 02:32:35 +0800

    访问规则设置

--- packages/h5-admin/src/pages/permission/access.tsx
+++ packages/h5-admin/src/pages/permission/access.tsx
@@ -1,86 +1,119 @@
-import { Button, Space, Table, Tree } from 'antd'
-import { useState } from 'react'
-
-const columns = [
-  {
-    title: '访问规则ID',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: '访问规则表达式',
-    dataIndex: 'accessRule',
-    key: 'accessRule',
-  },
-  {
-    title: '描述',
-    dataIndex: 'remark',
-    key: 'remark',
-  },
-  {
-    title: '访问规则类型',
-    dataIndex: 'accessType',
-    key: 'accessType',
-  },
-  {
-    title: '被引用次数',
-    dataIndex: 'permissionAssociationCount',
-    key: 'permissionAssociationCount',
-  },
-  {
-    title: '创建时间',
-    dataIndex: 'createdAt',
-    key: 'createdAt',
-  },
-  {
-    title: '操作',
-    key: 'action',
-    render: () => (
-      <Space size="middle">
-        <Button type="link">修改</Button>
-        <Button type="link">删除</Button>
-      </Space>
-    ),
-  },
-]
-
-const data = [
-  {
-    key: 1,
-    id: 1,
-    accessRule: 'post:/*',
-    remark: '全量权限',
-    accessType: 'action',
-    permissionAssociationCount: 1,
-    createdAt: '2022-01-01 22:22:22',
-  },
-  {
-    key: 2,
-    id: 2,
-    accessRule: 'sys_mobile:/passport',
-    remark: '移动端登录页面',
-    accessType: 'resource',
-    permissionAssociationCount: 1,
-    createdAt: '2022-01-01 22:22:22',
-  },
-  {
-    key: 3,
-    id: 3,
-    accessRule: 'post:/user/login',
-    remark: '登录帐号',
-    accessType: 'action',
-    permissionAssociationCount: 1,
-    createdAt: '2022-01-01 22:22:22',
-  },
-]
+import { useState, useRef, useCallback } from 'react'
+import { useMount } from 'ahooks'
+import { Button, Space, Table, Modal, message } from 'antd'
+import { ColumnsType } from 'antd/lib/table'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
+import { formatTimeStr } from '@shippo/sdk-utils'
+import { services, IPermissionAccess } from '@shippo/sdk-services'
+import { EditAccessDrawer, EditAccessDrawerRef } from './components/edit-access-drawer'
+const { confirm } = Modal
 
 export const Page_permission_access: React.FC = () => {
+  const [data, setData] = useState<IPermissionAccess[]>()
+  const editAccessDrawerRef = useRef<EditAccessDrawerRef>(null)
+
+  const handleDle = useCallback((id: number) => {
+    confirm({
+      title: '确认删除？',
+      icon: <ExclamationCircleOutlined />,
+      content: '此操作不可逆',
+      onOk() {
+        console.log('OK')
+        services.permissionAccess.del({ id }).then((hr) => {
+          if (hr.data.success) {
+            message.success('成功')
+          } else {
+            message.success('失败')
+          }
+        })
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+    })
+  }, [])
+
+  const [columns, setColumns] = useState<ColumnsType<IPermissionAccess>>([
+    {
+      title: '访问规则ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: '访问规则表达式',
+      dataIndex: 'accessRule',
+      key: 'accessRule',
+    },
+    {
+      title: '描述',
+      dataIndex: 'remark',
+      key: 'remark',
+    },
+    {
+      title: '访问规则类型',
+      dataIndex: 'accessType',
+      key: 'accessType',
+    },
+    {
+      title: '被引用次数',
+      dataIndex: 'permissionAssociationCount',
+      key: 'permissionAssociationCount',
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <Button
+            type="link"
+            onClick={() => {
+              editAccessDrawerRef.current?.open(record)
+            }}
+          >
+            修改
+          </Button>
+          <Button
+            type="link"
+            onClick={() => {
+              handleDle(record.id)
+            }}
+          >
+            删除
+          </Button>
+        </Space>
+      ),
+    },
+  ])
+
+  useMount(async () => {
+    const hr = await services.permissionAccess.find_all()
+    setData(
+      hr.data.resource.map((item) => {
+        return { ...item, createdAt: formatTimeStr(item.createdAt) }
+      })
+    )
+  })
+
   return (
     <div>
+      <EditAccessDrawer ref={editAccessDrawerRef} />
       <Space size="middle">
-        <Button type="primary">新增访问规则</Button>
+        <Button type="primary" onClick={() => editAccessDrawerRef.current?.open()}>
+          新增访问规则
+        </Button>
       </Space>
-      <Table columns={columns} dataSource={data} pagination={{ position: ['bottomCenter'] }} />
+      <Table
+        rowKey="id"
+        columns={columns}
+        dataSource={data}
+        pagination={{ position: ['bottomCenter'] }}
+        size="small"
+      />
     </div>
   )
 }
---
+++ packages/h5-admin/src/pages/permission/components/edit-access-drawer.tsx
@@ -0,0 +1,147 @@
+import React, { useImperativeHandle, useState, useCallback, useMemo } from 'react'
+import { Drawer, Space, Button, Form, Row, Col, Input, message } from 'antd'
+import { IPermissionAccess, services, __permissionAccess } from '@shippo/sdk-services'
+
+const __defaultAccess = __permissionAccess()
+
+export interface EditAccessDrawerRef {
+  open: (access?: IPermissionAccess) => void
+}
+
+export interface EditAccessDrawerProps {
+  onClose?: () => void
+}
+
+const Component: React.ForwardRefRenderFunction<EditAccessDrawerRef, EditAccessDrawerProps> = (
+  props,
+  ref
+) => {
+  const { onClose } = props
+  const [access, setAccess] = useState<IPermissionAccess>(__defaultAccess)
+
+  const [visible, setVisible] = useState(false)
+
+  const isUpdate = useMemo(() => {
+    return access.id !== 0
+  }, [access])
+
+  // ref
+  useImperativeHandle(ref, () => {
+    return {
+      // 打开抽屉
+      open: (access?: IPermissionAccess) => {
+        if (access) {
+          setAccess({ ...access })
+        } else {
+          setAccess(__permissionAccess())
+        }
+        setVisible(true)
+      },
+    }
+  })
+
+  // 关闭抽屉
+  const closeDrawer = useCallback(() => {
+    onClose && onClose()
+    setVisible(false)
+  }, [onClose])
+
+  // 更改access
+  const changeAccess = useCallback((access: Partial<IPermissionAccess>) => {
+    setAccess((old) => ({ ...old, ...access }))
+  }, [])
+
+  const handleSave = useCallback(async () => {
+    console.log(access)
+    if (isUpdate) {
+      const hr = await services.permissionAccess.update({
+        id: access.id,
+        accessRule: access.accessRule,
+        accessType: access.accessType,
+        remark: access.remark,
+      })
+      if (hr.data.success) {
+        message.success('成功')
+        closeDrawer()
+      } else {
+        message.success('失败')
+      }
+    } else {
+      const hr = await services.permissionAccess.create({
+        accessRule: access.accessRule,
+        accessType: access.accessType,
+        remark: access.remark,
+      })
+      if (hr.data.success) {
+        message.success('成功')
+        closeDrawer()
+      } else {
+        message.success('失败')
+      }
+    }
+  }, [isUpdate, access, closeDrawer])
+
+  return (
+    <Drawer
+      title={isUpdate ? '编辑访问规则' : '新增访问规则'}
+      width={720}
+      onClose={closeDrawer}
+      visible={visible}
+      bodyStyle={{ paddingBottom: 80 }}
+    >
+      <Form layout="vertical" requiredMark={false}>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="访问规则表达式"
+              rules={[{ required: true, message: '请输入访问规则表达式' }]}
+            >
+              <Input
+                placeholder="请输入访问规则表达式"
+                value={access.accessRule}
+                onChange={(event) => {
+                  changeAccess({ accessRule: event.currentTarget.value })
+                }}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="描述" rules={[{ required: true, message: '请输入描述' }]}>
+              <Input
+                placeholder="请输入描述"
+                value={access.remark}
+                onChange={(event) => {
+                  changeAccess({ remark: event.currentTarget.value })
+                }}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="访问规则类型"
+              rules={[{ required: true, message: '请输入访问规则类型' }]}
+            >
+              <Input
+                placeholder="请输入访问规则类型"
+                value={access.accessType}
+                onChange={(event) => {
+                  changeAccess({ accessType: event.currentTarget.value })
+                }}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item>
+          <Space>
+            <Button onClick={closeDrawer}>关闭</Button>
+            <Button onClick={handleSave} type="primary">
+              保存
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </Drawer>
+  )
+}
+
+export const EditAccessDrawer = React.forwardRef(Component)
