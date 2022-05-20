135a53572f65c94cc155620aedbb7abbbb75e453
kazura233 <kazira233@gmail.com>
2022-05-21 02:49:54 +0800

    eslint

--- packages/h5-mobile/vite.config.ts
+++ packages/h5-mobile/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
       '~': resolve(__dirname, 'src'),
     },
   },
-  plugins: [react()],
+  plugins: [eslint(), react()],
   server: {
     proxy: {
       '/api': {
