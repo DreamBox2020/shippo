caf0498b580cc869a515bf5d62a62cfaa7a62b06
kazura233 <kazira233@gmail.com>
2022-06-25 12:30:20 +0800

    types

---
+++ packages/types/types/index.d.ts
@@ -0,0 +1,90 @@
+export interface IRole {
+    id: number;
+    createdAt: string;
+    roleName: string;
+    remark: string;
+}
+export declare const __role: () => IRole;
+export interface IPermissionPolicy {
+    id: number;
+    createdAt: string;
+    policyName: string;
+    remark: string;
+}
+export declare const __permissionPolicy: () => IPermissionPolicy;
+export interface IPermissionAccess {
+    id: number;
+    createdAt: string;
+    accessRule: string;
+    remark: string;
+    accessType: string;
+}
+export declare const __permissionAccess: () => IPermissionAccess;
+export interface IUser {
+    id: number;
+    createdAt: string;
+    phone: string;
+    email: string;
+    nickname: string;
+    avatar: string;
+    exp: number;
+    coin: number;
+    role: number;
+    wxPassportId: number;
+}
+export declare const __user: () => IUser;
+export interface IPagination {
+    current: number;
+    pageSize: number;
+    total: number;
+}
+export interface IUserExtRoleName extends IUser {
+    roleName: string;
+}
+export declare const __userExtRoleName: () => IUserExtRoleName;
+export interface IWxArticle {
+    id: number;
+    createdAt: string;
+    title: string;
+    url: string;
+    image1: string;
+    image2: string;
+    commentSwitch: number;
+    offiaccountId: number;
+    wxPassportId: number;
+}
+export declare const __wxArticle: () => IWxArticle;
+export interface IWxArticleExtOffiaccountNickname extends IWxArticle {
+    offiaccountNickname: string;
+}
+export declare const __wxArticleExtOffiaccountNickname: () => IWxArticleExtOffiaccountNickname;
+export interface IPassport {
+    id: number;
+    createdAt: string;
+    token: string;
+    userId: number;
+    ip: string;
+    ua: string;
+    client: number;
+    wxPassportId: number;
+}
+export declare const __passport: () => IPassport;
+export interface IUserInfo {
+    access: Array<IPermissionAccess>;
+    user: IUser;
+    passport: string;
+    uid: number;
+}
+export declare const __userInfo: () => IUserInfo;
+export interface IWxComment {
+    id: number;
+    createdAt: string;
+    content: string;
+    articleId: number;
+    wxPassportId: number;
+    likeNum: number;
+    isElected: number;
+    isTop: number;
+    replyCommentId: number;
+}
+export declare const __wxComment: () => IWxComment;
